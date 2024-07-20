73303979815cb5c74215c976a06e2c31222ccea5
pswincher <preston.swincher@gmail.com>
2024-07-20 16:13:59 -0700

    final updates to project 9
    before submission?

---
+++ src/components/ModalConfirm.js
@@ -0,0 +1,72 @@
+import Modal from "./Modal.js"
+import { modalSelectors } from "../utils/constants.js";
+ 
+export default class ModalConfirm extends Modal {
+    constructor({ modalSelector, confirmText = "Confirm", awaitingConfirmText = "Confirming...", cancelText = "Cancel" } ={})
+    {
+        super({ modalSelector })
+        console.log(this._modalElement);
+        this._confirmText = confirmText;
+        this._awaitingConfirmText = awaitingConfirmText;
+        this._cancelText = cancelText;
+        this._modalConfirmButton = this._modalElement.querySelector(modalSelectors.modalConfirmButtonSelector);
+        this._modalCancelButton = this._modalElement.querySelector(modalSelectors.modalCancelButtonSelector);
+        this.awaitUserChoice = this.awaitUserChoice.bind(this);
+
+        this._setConfirmText(this._confirmText);
+        this._setCancelText(this._cancelText);
+    }
+
+    _setConfirmText(text) {
+        console.log(this._modalCinformButton);
+        this._modalConfirmButton.textContent = text;
+    }
+
+    _setCancelText(text) {
+        this._modalCancelButton.textContent = text;
+    }
+
+    _setEventListeners() {
+      }
+
+    _onCancelClick() {
+        this._modalElement.removeEventListener('click', this.handler);
+        this.close();
+        return false;
+    }
+
+    _onConfirmClick() {
+        this._modalElement.removeEventListener('click', this.handler);
+        this.close();
+        return true;
+    }
+
+    _setToConfirming() {
+        this._modalConfirmButton.textContent = this._awaitingConfirmText;
+    }
+
+    setToConfirm() {
+        this._modalConfirmButton.textContent = this._confirmText
+    }
+
+    awaitUserChoice() {
+        this.open();
+        const modal = this;
+        return new Promise(resolve => {
+            modal._modalElement.addEventListener('click', function clickHandler(evt) {
+                if (evt.target.classList.contains("modal__button-confirm")) {
+                    modal._setToConfirming();
+                    modal._modalElement.removeEventListener('click', clickHandler);
+                    resolve(true);
+                }
+                if (evt.target.classList.contains("modal__button-cancel")) {
+                    modal._modalElement.removeEventListener('click', clickHandler);
+                    modal.close();
+                    resolve(false);
+                }  
+            });
+        })
+    }
+
+    
+}
